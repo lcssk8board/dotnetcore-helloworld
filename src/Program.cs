25509d3026786d1b003960f6949e867d184e2499
Lucas Cé Bertin <lcs.sk8board@gmail.com>
2016-05-27 19:53:58 -0300

    added tests with .netcore and docker

---
+++ src/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MainAsync(args).Wait();
+        }
+        
+        public async static Task MainAsync(string[] args)
+        {
+            await Console.Out.WriteLineAsync("Hello World!");
+        }
+    }
+}
---
+++ src/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+clear 
+
+docker build -t hwapp .
